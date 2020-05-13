2718ff1be0999efa917214c783b398608777987a
maciexu <43970987+maciexu@users.noreply.github.com>
2020-05-13 15:00:58 -0400

    Create Creating_and_Manipulating_your_own_Databases.py

---
+++ Creating_and_Manipulating_your_own_Databases.py
@@ -0,0 +1,58 @@
+""" Creating tables with SQLAlchemy """
+# Import Table, Column, String, Integer, Float, Boolean from sqlalchemy
+from sqlalchemy import Table, Column, String, Integer, Float, Boolean
+
+# Define a new table with a name, count, amount, and valid column: data
+data = Table('data', metadata,
+             Column('name', String(255), unique=True),
+             Column('count', Integer(), default=1),
+             Column('amount', Float()),
+             Column('valid', Boolean(), default=False)
+)
+
+# Use the metadata to create the table
+metadata.create_all(engine)
+
+# Print table details
+print(repr(data))
+
+
+
+""" Insert """
+# Example 1
+# Import insert and select from sqlalchemy
+from sqlalchemy import insert, select
+
+# Build an insert statement to insert a record into the data table: insert_stmt
+insert_stmt = insert(data).values(name='Anna', count=1, amount=1000.00, valid=True)
+
+# Execute the insert statement via the connection: results
+results = connection.execute(insert_stmt)
+
+# Print result rowcount
+print(results.rowcount)
+
+# Build a select statement to validate the insert: select_stmt
+select_stmt = select([data]).where(data.columns.name == 'Anna')
+
+# Print the result of executing the query.
+print(connection.execute(select_stmt).first())
+
+
+# Example 2
+# Build a list of dictionaries: values_list
+values_list = [
+    {'name':'Anna', 'count':1, 'amount':1000.00, 'valid':True},
+    {'name':'Taylor', 'count':1, 'amount':750.00, 'valid':False}
+]
+
+# Build an insert statement for the data table: stmt
+stmt = insert(data)
+
+# Execute stmt with the values_list: results
+results = connection.execute(stmt, values_list)
+
+# Print rowcount
+print(results.rowcount)
+
+
