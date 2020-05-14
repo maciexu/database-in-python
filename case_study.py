1ed4b8a360545324c3e87413993b359f23ea8488
maciexu <43970987+maciexu@users.noreply.github.com>
2020-05-13 21:50:11 -0400

    Create case_study.py

---
+++ case_study.py
@@ -0,0 +1,125 @@
+#1. Setup the engine and metadata
+from sqlalchemy import create_engine, MetaData
+
+# Define an engine to connect to chapter5.sqlite: engine
+engine = create_engine('sqlite:///chapter5.sqlite')
+
+# Initialize MetaData: metadata
+metadata = MetaData()
+
+#2. Create the table to the database
+# Import Table, Column, String, and Integer
+from sqlalchemy import Table, Column, String, Integer
+
+# Build a census table: census
+census = Table('census', metadata,
+               Column('state', String(30)),
+               Column('sex', String(1)),
+               Column('age', Integer()),
+               Column('pop2000', Integer()),
+               Column('pop2008', Integer()))
+
+# Create the table in the database
+metadata.create_all(engine)
+
+
+#3. Reading the data from the CSV
+# Create an empty list: values_list
+values_list = []
+
+# Iterate over the rows
+for row in csv_reader:
+    # Create a dictionary with the values
+    data = {'state': row[0], 'sex': row[1], 'age': row[2], 'pop2000': row[3],
+            'pop2008': row[4]}
+    # Append the dictionary to the values list
+    values_list.append(data)
+    
+#4. Load data from a list into the Table
+# Import insert
+from sqlalchemy import insert
+
+# Build insert statement: stmt
+stmt = insert(census)
+
+# Use values_list to insert data: results
+results = connection.execute(stmt, values_list)
+
+# Print rowcount
+print(results.rowcount)
+
+
+#Task 1. Determine the average age by population
+"""
+To calculate a weighted average, we first find the total sum of weights multiplied by the values we're averaging, 
+then divide by the sum of all the weights.
+
+For example, if we wanted to find a weighted average of data = [10, 30, 50] weighted by weights = [2,4,6], 
+we would compute 2⋅10+4⋅30+6⋅502+4+6, or sum(weights * data) / sum(weights).
+"""
+# Import select and func
+from sqlalchemy import select, func
+
+# Select sex and average age weighted by 2000 population
+stmt = select([(func.sum(census.columns.pop2000 * census.columns.age) 
+  					/ func.sum(census.columns.pop2000)).label('average_age'),
+               census.columns.sex
+			  ])
+
+# Group by sex
+stmt = stmt.group_by(census.columns.sex)
+
+# Execute the query and fetch all the results
+results = connection.execute(stmt).fetchall()
+
+# Print the sex and average age column for each result
+for x in results:
+    print(x.sex, x.average_age)
+
+
+#Task 2. Determine the percentage of population by gender(women) and state
+# import case, cast and Float from sqlalchemy
+from sqlalchemy import case, cast, Float
+
+# Build a query to calculate the percentage of women in 2000: stmt
+stmt = select([census.columns.state,
+    (func.sum(
+        case([
+            (census.columns.sex == 'F', census.columns.pop2000)
+        ], else_=0)) /
+     cast(func.sum(census.columns.pop2000), Float) * 100).label('percent_female')
+])
+
+# Group By state
+stmt = stmt.group_by('state')
+
+# Execute the query and store the results: results
+results = connection.execute(stmt).fetchall()
+
+# Print the percentage
+for result in results:
+    print(result.state, result.percent_female)
+
+
+# Task 3. Determine the difference by state from the 2000 and 2008 censuses
+# Build query to return state name and population difference from 2008 to 2000
+stmt = select([census.columns.state,
+     (census.columns.pop2008-census.columns.pop2000).label('pop_change')
+])
+
+# Group by State
+stmt = stmt.group_by(census.columns.state)
+
+# Order by Population Change
+stmt = stmt.order_by(desc('pop_change'))
+
+# Limit to top 10
+stmt = stmt.limit(10)
+
+# Use connection to execute the statement and fetch all results
+results = connection.execute(stmt).fetchall()
+
+# Print the state and population change for each record
+for result in results:
+    print('{}:{}'.format(result.state, result.pop_change))
+
